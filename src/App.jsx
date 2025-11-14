c51ede112c550c25e52775fa67b11457570f2fb2
SantiRuiz7 <ruizsantiagodavid@gmail.com>
2025-11-13 23:41:31 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+      <div className="p-6 bg-green text-white text-3xl font-bold">
+      ¡Tailwind 4 + Vite funcionando! 🚀
+    </div>
+  )
+}
+
+export default App
